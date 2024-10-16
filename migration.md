a759446f659694097ab2b59c968e79084fd77d19
Reza Faraji <faaji@kzvn.de>
2024-10-16 11:32:34 +0200

    added new components

---
+++ migration.md
@@ -0,0 +1,8 @@
+# Migration skripts
+
+## Conditional control flow migration
+```
+ng g @angular/core:control-flow
+```
+
+## 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { DashboardComponent } from './books/dashboard/dashboard.component';
 import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from '../components/navbar/navbar.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [DashboardComponent, RouterOutlet]
+    imports: [DashboardComponent, RouterOutlet, NavbarComponent]
 })
 export class AppComponent {
   title = 'Book Rating';
