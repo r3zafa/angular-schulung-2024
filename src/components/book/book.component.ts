19950757156ddaa17ff8f3a90351f540adc9c43b
Reza Faraji <r3zafaraji@gmail.com>
2024-10-16 14:04:04 +0200

    use signal input

--- src/components/book/book.component.ts
+++ src/components/book/book.component.ts
@@ -1,27 +1,27 @@
 import { CurrencyPipe, NgIf } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, input, Output } from '@angular/core';
 
 import { Book } from '../../shared/book';
 
 @Component({
-    selector: 'app-book',
-    standalone: true,
-    templateUrl: './book.component.html',
-    styleUrl: './book.component.scss',
-    imports: [NgIf, CurrencyPipe]
+  selector: 'app-book',
+  standalone: true,
+  templateUrl: './book.component.html',
+  styleUrl: './book.component.scss',
+  imports: [NgIf, CurrencyPipe]
 })
 export class BookComponent {
 
-  @Input() book?: Book;
+  book = input<Book>();
 
   @Output() rateUp = new EventEmitter<Book>();
   @Output() rateDown = new EventEmitter<Book>();
 
   doRateUp() {
-    this.rateUp.emit(this.book);
+    //this.rateUp.emit(this.book);
   }
 
   doRateDown() {
-    this.rateDown.emit(this.book);
+    //this.rateDown.emit(this.book);
   }
 }
