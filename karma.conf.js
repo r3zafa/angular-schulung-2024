bd7a2d88b69635e5a64d0429629bfc3b058fd053
Reza Faraji <r3zafaraji@gmail.com>
2024-10-17 14:43:12 +0200

    test 1. variant

--- karma.conf.js
+++ karma.conf.js
@@ -1,36 +1,57 @@
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+console.warn(process.env.CHROME_BIN);
 module.exports = function (config) {
   config.set({
     basePath: "",
-    frameworks: ["jasmine", '@angular-devkit/build-angular'],
-    reporters: ["progress", "kjhtml"],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    singleRun: false,
+    restartOnFileChange: true,
+    singleRun: false,
+    concurrency: Infinity,
+    captureTimeout: 210000,
+    browserDisconnectTolerance: 3,
+    browserDisconnectTimeout: 210000,
+    browserNoActivityTimeout: 210000,
+    client: {
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+    },
+    frameworks: [
+      "jasmine",
+      '@angular-devkit/build-angular'
+    ],
+    reporters: [
+      "progress",
+      "kjhtml",
+      'coverage'
+    ],
     plugins: [
       require("karma-jasmine"),
       require("karma-chrome-launcher"),
       require("karma-jasmine-html-reporter"),
       require("karma-firefox-launcher"),
       require("@angular-devkit/build-angular/plugins/karma"),
+      'karma-coverage'
     ],
-    client: {
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
-    },
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ["FirefoxHeadless"],
-    singleRun: true,
-    concurrency: Infinity,
+    browsers: [
+      "Chrome",
+      'ChromeHeadless'
+    ],
     customLaunchers: {
       ChromeHeadless: {
-        base: "ChromeHeadless",
-        flags: ["--no-sandbox",'--disable-dev-shm-usage'],
+        base: "Chrome",
+        flags: ["--no-sandbox", '--disable-dev-shm-usage'],
       },
       FirefoxHeadless: {
         base: "Firefox",
         flags: ["--headless"],
       },
     },
-    singleRun: false,
-    restartOnFileChange: true
+    coverageReporter: {
+      type: 'html', // You can also specify other formats like 'lcov', 'text-summary', etc.
+      dir: 'coverage/',
+    },
   });
 };
--- src/components/dashboard/dashboard.component.spec.ts
+++ src/components/dashboard/dashboard.component.spec.ts
@@ -3,32 +3,57 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Book } from '../../shared/book';
 import { BookRatingService } from '../../shared/book-rating.service';
 import { DashboardComponent } from './dashboard.component';
+import { of } from 'rxjs';
+import { BookStoreService } from '../../shared/book-store.service';
+import { RouterTestingHarness } from '@angular/router/testing';
 
-describe('DashboardComponent', () => {
+fdescribe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(async () => {
 
+    // 1. strategie -- alle abhängigkeiten durch etwas ersetzen, das ich erzeugt habe.
+
     const bookRatingMock = {
       rateUp: (book: Book) => book
     };
 
+    const bookStoreMock = {
+      getAllBooks: () => of([{ isbn: '', title: 'Hallo Hannover', description: '', rating: 5, price: 1 } as Book])
+    }
+
+
     await TestBed.configureTestingModule({
-    imports: [DashboardComponent],
-    providers: [{
+      imports: [DashboardComponent],
+      providers: [{
         provide: BookRatingService,
         useValue: bookRatingMock
-    }]
-})
-    .compileComponents();
+      }, {
+        provide: BookStoreService,
+        useValue: bookStoreMock
+      },
+    ]
+    })
+      .compileComponents();
 
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('TODO', () => {
-    expect(true).toBeTruthy();
+  // 1. prüfung ob dieser wert im array zu finden ist.
+  it('should show the title of the books', () => {
+    component.books()[0].title === 'Hallo Hannover';
   });
+
+
+  // wir prüfen auch noch einmal das html, ob der wert wirklich gebunden
+  // frage: wollen wir binding prüfen
+  // mögliche antwort: nein wir prüfen keine bindings in unserem projekt ---> dann bitte e2e als ergänzung
+  // mögliche antwort: ja, dann so:
+  it('should show the title of the books in gui', () => {
+    expect(fixture.nativeElement.querySelector('h2')?.textContent).toContain('Hallo Hannover');
+  });
+
 });
