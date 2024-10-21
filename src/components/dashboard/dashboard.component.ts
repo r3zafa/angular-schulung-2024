57dd8998374f66bb430068c75f3a0d4e3389a944
Reza Faraji <r3zafaraji@gmail.com>
2024-10-21 13:47:44 +0200

    ngrx load data  in dashboard

--- src/components/dashboard/dashboard.component.ts
+++ src/components/dashboard/dashboard.component.ts
@@ -6,8 +6,9 @@ import { BookRatingService } from '../../shared/book-rating.service';
 import { BookStoreService } from '../../shared/book-store.service';
 import { MatButtonModule } from '@angular/material/button';
 import { BookFormComponent } from "../book-form/book-form.component";
-import { B } from '@angular/cdk/keycodes';
 import { switchMap } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { selectBooks, selectBooksLoading } from '../../store/book/book.selectors';
 
 @Component({
   selector: 'k-dashboard',
@@ -19,34 +20,31 @@ import { switchMap } from 'rxjs';
 })
 export class DashboardComponent {
   // injects
-  private rs: BookRatingService = inject(BookRatingService);
-  private bs = inject(BookStoreService);
+  protected store = inject(Store);
   // signals
-  books: WritableSignal<Book[]> = signal<Book[]>([]);
+  books = this.store.selectSignal(selectBooks);
+  loading = this.store.selectSignal(selectBooksLoading);
   currentBook = signal<Book | undefined>(undefined);
-  // 2. weg (convert to signal) für migration geeignet. am besten nicht verwenden.
-  // books$ = this.bs.getAllBooks();
-  // books = toSignal(this.books$, {initialValue: []})
 
   // vars
   toggleForm: boolean = false;
 
   constructor() {
-    this.bs.getAllBooks().subscribe(books => this.books.set(books));
+
   }
 
   showFirstBook() {
-    alert(this.books()[0]?.title);
+
   }
 
   doRateUp(book: Book) {
-    const ratedBook = this.rs.rateUp(book);
-    this.updateAndSortList(ratedBook);
+    //const ratedBook = this.rs.rateUp(book);
+    //this.updateAndSortList(ratedBook);
   }
 
   doRateDown(book: Book) {
-    const ratedBook = this.rs.rateDown(book);
-    this.updateAndSortList(ratedBook);
+    //const ratedBook = this.rs.rateDown(book);
+    //this.updateAndSortList(ratedBook);
   }
 
   updateAndSortList(ratedBook: Book) {
@@ -64,9 +62,9 @@ export class DashboardComponent {
 
     // oder update
 
-    this.books.update(books => books
-      .map(b => b.isbn === ratedBook.isbn ? ratedBook : b)
-      .sort((a, b) => b.rating - a.rating));
+    //this.books.update(books => books
+      //.map(b => b.isbn === ratedBook.isbn ? ratedBook : b)
+      //.sort((a, b) => b.rating - a.rating));
 
     // TODO: buch zum serve senden (Hausaufgabe)
   }
@@ -77,20 +75,20 @@ export class DashboardComponent {
 
 
   addBook(book: Book) {
-    this.books.update(books => [...books,book])
+    //this.books.update(books => [...books,book])
   }
 
   changeToEditMode(book: Book) {
-    this.currentBook.set(book);
-    this.toggleForm = true;
+    //this.currentBook.set(book);
+   // this.toggleForm = true;
   }
 
   changeBook(cBook: Book) {
     // this.updateAndSortList(cBook);
-    this.currentBook.set(undefined);
+    //this.currentBook.set(undefined);
 
-    this.bs.updateBook(cBook)
-    .pipe(switchMap(_ => this.bs.getAllBooks()))
-    .subscribe(books => this.books.set(books));
+    //this.bs.updateBook(cBook)
+    //.pipe(switchMap(_ => this.bs.getAllBooks()))
+    //.subscribe(books => this.books.set(books));
   }
 }
--- src/store/book/book.effects.ts
+++ src/store/book/book.effects.ts
@@ -1,25 +1,29 @@
 import { inject, Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { catchError, map, concatMap } from 'rxjs/operators';
-import { EMPTY, of } from 'rxjs';
+import { of } from 'rxjs';
 import { BookActions } from './book.actions';
+import { BookStoreService } from '../../shared/book-store.service';
 
 
 @Injectable()
 export class BookEffects {
 
+  bs = inject(BookStoreService);
+
   loadBooks$ = createEffect(() => {
     return inject(Actions).pipe(
+
       ofType(BookActions.loadBooks),
       concatMap(() =>
-        /** An EMPTY observable only emits completion. Replace with your own observable API request */
-        EMPTY.pipe(
+        this.bs.getAllBooks().pipe(
           map(books => BookActions.loadBooksSuccess({ books })),
           catchError(error => of(BookActions.loadBooksFailure({ error }))))
       )
+
     );
   });
 
 
-  constructor(private actions$: Actions) {}
+  constructor(private actions$: Actions) { }
 }
