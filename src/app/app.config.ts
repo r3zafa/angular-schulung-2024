aace53f8c8275297c483935d963cce1c33cd718d
Reza Faraji <r3zafaraji@gmail.com>
2024-10-21 10:39:06 +0200

    added ngRx

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,14 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection  } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideState, provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { bookFeature } from '../store/book/book.reducer';
+import { BookEffects } from '../store/book/book.effects';
 
 
 export const appConfig: ApplicationConfig = {
@@ -11,6 +16,18 @@ export const appConfig: ApplicationConfig = {
     //provideZoneChangeDetection({ eventCoalescing: true }),
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes /*, withDebugTracing()*/),
-    provideHttpClient(), provideAnimationsAsync(), provideAnimationsAsync()
+    provideHttpClient(),
+    provideAnimationsAsync(),
+    provideAnimationsAsync(),
+
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({
+      maxAge: 25, logOnly: !isDevMode(),
+    }),
+
+    // new
+    provideState(bookFeature),
+    provideEffects(BookEffects),
   ]
 };
