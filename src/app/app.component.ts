3ee79b546d2de91e17e4970c6d1c904f0f065ad5
Reza Faraji <r3zafaraji@gmail.com>
2024-10-21 11:39:51 +0200

    actions effect and app init redux

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { DashboardComponent } from '../components/dashboard/dashboard.component';
 import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from '../components/navbar/navbar.component';
+import { Store } from '@ngrx/store';
+import { BookActions } from '../store/book/book.actions';
 
 @Component({
     selector: 'app-root',
@@ -12,4 +14,8 @@ import {NavbarComponent} from '../components/navbar/navbar.component';
 })
 export class AppComponent {
   title = 'Book Rating';
+
+  constructor(store:Store) {
+    store.dispatch(BookActions.loadBooks());
+  }
 }
