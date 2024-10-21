e072a96af38980cae547efad728e6ca85d1641df
Reza Faraji <r3zafaraji@gmail.com>
2024-10-21 10:54:07 +0200

    update book reducer

--- src/store/book/book.reducer.ts
+++ src/store/book/book.reducer.ts
@@ -1,14 +1,17 @@
 import { createFeature, createReducer, on } from '@ngrx/store';
 import { BookActions } from './book.actions';
+import { Book } from '../../shared/book';
 
 export const bookFeatureKey = 'book';
 
 export interface State {
-
+  book: Book[],
+  loading:boolean
 }
 
 export const initialState: State = {
-
+  book: [],
+  loading: false
 };
 
 export const reducer = createReducer(
